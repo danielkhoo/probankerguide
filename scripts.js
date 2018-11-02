ce6feafa1ee25acb32778e494ff9d15b5a0bc3ed
XavierTayJH <xaviertayjh@gmail.com>
2018-11-02 15:57:51 +0800

    Add accordion to balance sheet

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-// When the user scrolls the page, execute myFunction 
+// When the user scrolls the page, execute myFunction
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
