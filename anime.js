f43623d3cc56ec6ec5250c645f2f59496911619c
16sic013j <16sic013@sit.singaporetech.edu.sg>
2018-11-05 21:25:52 +0800

    Added animation js lib

---
+++ anime.js
@@ -0,0 +1,14 @@
+$(window).scroll(function() {
+
+  	if (isScrolledIntoView("#section2")) {
+
+      anime({
+        targets: document.getElementById('section2'),
+        easing: 'easeOutExpo',
+        loop: false
+      });
+
+       return;
+     }
+     
+});
