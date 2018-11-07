d1527915695091d34cb59cd8d08f15f337cec300
Nurulhuda Nasir <1602623@sit.singaporetech.edu.sg>
2018-11-08 05:06:43 +0800

    Minor edit to JS

--- js/scripts.js
+++ js/scripts.js
@@ -22,18 +22,6 @@ function isScrolledIntoView(elem)
   return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
 }
 
-$(window).scroll(function() {
-  if (isScrolledIntoView("#section1")) { window.history.pushState("state", "title", "#start"); return;	}
-  if (isScrolledIntoView("#section2")) { window.history.pushState("state", "title", "#welcome"); return; }
-  if (isScrolledIntoView("#section3")) { window.history.pushState("state", "title", "#gameplay"); return; }
-  if (isScrolledIntoView("#section4")) { window.history.pushState("state", "title", "#learning"); return; }
-  if (isScrolledIntoView("#section5")) { window.history.pushState("state", "title", "#gamemodes"); return; }
-  if (isScrolledIntoView("#section6")) { window.history.pushState("state", "title", "#decisions"); return; }
-  if (isScrolledIntoView("#section7")) { window.history.pushState("state", "title", "#teamwork"); return; }
-  if (isScrolledIntoView("#section8")) { window.history.pushState("state", "title", "#balancesheet"); return; }
-  if (isScrolledIntoView("#section9")) { window.history.pushState("state", "title", "#notes"); return; }
-});
-
 function openCity(evt, cityName) {
   // Declare all variables
   var i, tabcontent, tablinks;
@@ -54,109 +42,119 @@ function openCity(evt, cityName) {
   document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+
 $(document).scroll(function () {
+	if (isScrolledIntoView("#section1")) { window.history.pushState("state", "title", "#start"); return;	}
+	if (isScrolledIntoView("#section2")) { window.history.pushState("state", "title", "#welcome"); return; }
+	if (isScrolledIntoView("#section3")) { window.history.pushState("state", "title", "#gameplay"); return; }
+	if (isScrolledIntoView("#section4")) { window.history.pushState("state", "title", "#learning"); return; }
+	if (isScrolledIntoView("#section5")) { window.history.pushState("state", "title", "#gamemodes"); return; }
+	if (isScrolledIntoView("#section6")) { window.history.pushState("state", "title", "#decisions"); return; }
+	if (isScrolledIntoView("#section7")) { window.history.pushState("state", "title", "#teamwork"); return; }
+	if (isScrolledIntoView("#section8")) { window.history.pushState("state", "title", "#balancesheet"); return; }
+	if (isScrolledIntoView("#section9")) { window.history.pushState("state", "title", "#notes"); return; }
 
-  var scroll_top = $(document).scrollTop();
-  var one_top = $('#section1').position().top;
-  var two_top = $('#section2').position().top;
-  var three_top = $('#section3').position().top;
-  var four_top = $('#section4').position().top;
-  var five_top = $('#section5').position().top;
-  var six_top = $('#section6').position().top;
-  var seven_top = $('#section7').position().top;
-  var eight_top = $('#section8').position().top;
-  var nine_top = $('#section9').position().top;
+	var scroll_top = $(document).scrollTop();
+	var one_top = $('#section1').position().top;
+	var two_top = $('#section2').position().top;
+	var three_top = $('#section3').position().top;
+	var four_top = $('#section4').position().top;
+	var five_top = $('#section5').position().top;
+	var six_top = $('#section6').position().top;
+	var seven_top = $('#section7').position().top;
+	var eight_top = $('#section8').position().top;
+	var nine_top = $('#section9').position().top;
 
-  if (scroll_top > one_top - 100 && scroll_top < two_top - 100) {
-    $('a[href="#section1"]').addClass('active');
-    $('a[href="#section2"]').removeClass('active');
-    $('a[href="#section3"]').removeClass('active');
-    $('a[href="#section4"]').removeClass('active');
-    $('a[href="#section5"]').removeClass('active');
-    $('a[href="#section6"]').removeClass('active');
-    $('a[href="#section7"]').removeClass('active');
-    $('a[href="#section8"]').removeClass('active');
-    $('a[href="#section9"]').removeClass('active');
-  } else if (scroll_top > two_top - 100 && scroll_top < three_top - 100) {
-    $('a[href="#section1"]').removeClass('active');
-    $('a[href="#section2"]').addClass('active');
-    $('a[href="#section3"]').removeClass('active');
-    $('a[href="#section4"]').removeClass('active');
-    $('a[href="#section5"]').removeClass('active');
-    $('a[href="#section6"]').removeClass('active');
-    $('a[href="#section7"]').removeClass('active');
-    $('a[href="#section8"]').removeClass('active');
-    $('a[href="#section9"]').removeClass('active');
-  } else if (scroll_top > three_top - 100 && scroll_top < four_top - 100) {
-    $('a[href="#section1"]').removeClass('active');
-    $('a[href="#section2"]').removeClass('active');
-    $('a[href="#section3"]').addClass('active');
-    $('a[href="#section4"]').removeClass('active');
-    $('a[href="#section5"]').removeClass('active');
-    $('a[href="#section6"]').removeClass('active');
-    $('a[href="#section7"]').removeClass('active');
-    $('a[href="#section8"]').removeClass('active');
-    $('a[href="#section9"]').removeClass('active');
-  } else if (scroll_top > four_top - 100 && scroll_top < five_top - 100) {
-    $('a[href="#section1"]').removeClass('active');
-    $('a[href="#section2"]').removeClass('active');
-    $('a[href="#section3"]').removeClass('active');
-    $('a[href="#section4"]').addClass('active');
-    $('a[href="#section5"]').removeClass('active');
-    $('a[href="#section6"]').removeClass('active');
-    $('a[href="#section7"]').removeClass('active');
-    $('a[href="#section8"]').removeClass('active');
-    $('a[href="#section9"]').removeClass('active');
-  } else if (scroll_top > five_top - 100 && scroll_top < six_top - 100) {
-    $('a[href="#section1"]').removeClass('active');
-    $('a[href="#section2"]').removeClass('active');
-    $('a[href="#section3"]').removeClass('active');
-    $('a[href="#section4"]').removeClass('active');
-    $('a[href="#section5"]').addClass('active');
-    $('a[href="#section6"]').removeClass('active');
-    $('a[href="#section7"]').removeClass('active');
-    $('a[href="#section8"]').removeClass('active');
-    $('a[href="#section9"]').removeClass('active');
-  } else if (scroll_top > six_top - 100 && scroll_top < seven_top - 100) {
-    $('a[href="#section1"]').removeClass('active');
-    $('a[href="#section2"]').removeClass('active');
-    $('a[href="#section3"]').removeClass('active');
-    $('a[href="#section4"]').removeClass('active');
-    $('a[href="#section5"]').removeClass('active');
-    $('a[href="#section6"]').addClass('active');
-    $('a[href="#section7"]').removeClass('active');
-    $('a[href="#section8"]').removeClass('active');
-    $('a[href="#section9"]').removeClass('active');
-  } else if (scroll_top > seven_top - 100 && scroll_top < eight_top - 100) {
-    $('a[href="#section1"]').removeClass('active');
-    $('a[href="#section2"]').removeClass('active');
-    $('a[href="#section3"]').removeClass('active');
-    $('a[href="#section4"]').removeClass('active');
-    $('a[href="#section5"]').removeClass('active');
-    $('a[href="#section6"]').removeClass('active');
-    $('a[href="#section7"]').addClass('active');
-    $('a[href="#section8"]').removeClass('active');
-    $('a[href="#section9"]').removeClass('active');
-  } else if (scroll_top > eight_top - 100 && scroll_top < nine_top - 100) {
-    $('a[href="#section1"]').removeClass('active');
-    $('a[href="#section2"]').removeClass('active');
-    $('a[href="#section3"]').removeClass('active');
-    $('a[href="#section4"]').removeClass('active');
-    $('a[href="#section5"]').removeClass('active');
-    $('a[href="#section6"]').removeClass('active');
-    $('a[href="#section7"]').removeClass('active');
-    $('a[href="#section8"]').addClass('active');
-    $('a[href="#section9"]').removeClass('active');
-  } else if (scroll_top > nine_top - 100) {
-    $('a[href="#section1"]').removeClass('active');
-    $('a[href="#section2"]').removeClass('active');
-    $('a[href="#section3"]').removeClass('active');
-    $('a[href="#section4"]').removeClass('active');
-    $('a[href="#section5"]').removeClass('active');
-    $('a[href="#section6"]').removeClass('active');
-    $('a[href="#section7"]').removeClass('active');
-    $('a[href="#section8"]').removeClass('active');
-    $('a[href="#section9"]').addClass('active');
-  }
+	if (scroll_top > one_top - 100 && scroll_top < two_top - 100) {
+		$('a[href="#section1"]').addClass('active');
+		$('a[href="#section2"]').removeClass('active');
+		$('a[href="#section3"]').removeClass('active');
+		$('a[href="#section4"]').removeClass('active');
+		$('a[href="#section5"]').removeClass('active');
+		$('a[href="#section6"]').removeClass('active');
+		$('a[href="#section7"]').removeClass('active');
+		$('a[href="#section8"]').removeClass('active');
+		$('a[href="#section9"]').removeClass('active');
+	} else if (scroll_top > two_top - 100 && scroll_top < three_top - 100) {
+		$('a[href="#section1"]').removeClass('active');
+		$('a[href="#section2"]').addClass('active');
+		$('a[href="#section3"]').removeClass('active');
+		$('a[href="#section4"]').removeClass('active');
+		$('a[href="#section5"]').removeClass('active');
+		$('a[href="#section6"]').removeClass('active');
+		$('a[href="#section7"]').removeClass('active');
+		$('a[href="#section8"]').removeClass('active');
+		$('a[href="#section9"]').removeClass('active');
+	} else if (scroll_top > three_top - 100 && scroll_top < four_top - 100) {
+		$('a[href="#section1"]').removeClass('active');
+		$('a[href="#section2"]').removeClass('active');
+		$('a[href="#section3"]').addClass('active');
+		$('a[href="#section4"]').removeClass('active');
+		$('a[href="#section5"]').removeClass('active');
+		$('a[href="#section6"]').removeClass('active');
+		$('a[href="#section7"]').removeClass('active');
+		$('a[href="#section8"]').removeClass('active');
+		$('a[href="#section9"]').removeClass('active');
+	} else if (scroll_top > four_top - 100 && scroll_top < five_top - 100) {
+		$('a[href="#section1"]').removeClass('active');
+		$('a[href="#section2"]').removeClass('active');
+		$('a[href="#section3"]').removeClass('active');
+		$('a[href="#section4"]').addClass('active');
+		$('a[href="#section5"]').removeClass('active');
+		$('a[href="#section6"]').removeClass('active');
+		$('a[href="#section7"]').removeClass('active');
+		$('a[href="#section8"]').removeClass('active');
+		$('a[href="#section9"]').removeClass('active');
+	} else if (scroll_top > five_top - 100 && scroll_top < six_top - 100) {
+		$('a[href="#section1"]').removeClass('active');
+		$('a[href="#section2"]').removeClass('active');
+		$('a[href="#section3"]').removeClass('active');
+		$('a[href="#section4"]').removeClass('active');
+		$('a[href="#section5"]').addClass('active');
+		$('a[href="#section6"]').removeClass('active');
+		$('a[href="#section7"]').removeClass('active');
+		$('a[href="#section8"]').removeClass('active');
+		$('a[href="#section9"]').removeClass('active');
+	} else if (scroll_top > six_top - 100 && scroll_top < seven_top - 100) {
+		$('a[href="#section1"]').removeClass('active');
+		$('a[href="#section2"]').removeClass('active');
+		$('a[href="#section3"]').removeClass('active');
+		$('a[href="#section4"]').removeClass('active');
+		$('a[href="#section5"]').removeClass('active');
+		$('a[href="#section6"]').addClass('active');
+		$('a[href="#section7"]').removeClass('active');
+		$('a[href="#section8"]').removeClass('active');
+		$('a[href="#section9"]').removeClass('active');
+	} else if (scroll_top > seven_top - 100 && scroll_top < eight_top - 100) {
+		$('a[href="#section1"]').removeClass('active');
+		$('a[href="#section2"]').removeClass('active');
+		$('a[href="#section3"]').removeClass('active');
+		$('a[href="#section4"]').removeClass('active');
+		$('a[href="#section5"]').removeClass('active');
+		$('a[href="#section6"]').removeClass('active');
+		$('a[href="#section7"]').addClass('active');
+		$('a[href="#section8"]').removeClass('active');
+		$('a[href="#section9"]').removeClass('active');
+	} else if (scroll_top > eight_top - 100 && scroll_top < nine_top - 100) {
+		$('a[href="#section1"]').removeClass('active');
+		$('a[href="#section2"]').removeClass('active');
+		$('a[href="#section3"]').removeClass('active');
+		$('a[href="#section4"]').removeClass('active');
+		$('a[href="#section5"]').removeClass('active');
+		$('a[href="#section6"]').removeClass('active');
+		$('a[href="#section7"]').removeClass('active');
+		$('a[href="#section8"]').addClass('active');
+		$('a[href="#section9"]').removeClass('active');
+	} else if (scroll_top > nine_top - 100) {
+		$('a[href="#section1"]').removeClass('active');
+		$('a[href="#section2"]').removeClass('active');
+		$('a[href="#section3"]').removeClass('active');
+		$('a[href="#section4"]').removeClass('active');
+		$('a[href="#section5"]').removeClass('active');
+		$('a[href="#section6"]').removeClass('active');
+		$('a[href="#section7"]').removeClass('active');
+		$('a[href="#section8"]').removeClass('active');
+		$('a[href="#section9"]').addClass('active');
+	}
 
 });
