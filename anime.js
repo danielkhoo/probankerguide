da4e87d58e1fa4b8e0259441a992420672552d86
16sic013j <16sic013@sit.singaporetech.edu.sg>
2018-11-07 22:04:25 +0800

    Added CSS animator framework and frontpage transition

--- anime.js
+++ anime.js
@@ -1,4 +1,27 @@
+$('#sec1_h1').toggleClass('animated bounceInDown');
+$('#sec1_h2').toggleClass('animated bounceInLeft');
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+ async function demo() {
+   $('#sec1_butt').toggleClass('animated bounceInLeft');
+   await sleep(2000);
+   $('#sec1_butt').toggleClass('animated bounceInLeft');
+    $('#sec1_butt').toggleClass('animated pulse infinite');
+ }
+demo();
+
+
+
+
 $(window).scroll(function() {
+
+  if (isScrolledIntoView("#section1")) {
+
+
+
+     return;
+   }
 
   	if (isScrolledIntoView("#section2")) {
 
