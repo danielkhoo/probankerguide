a373573c52d1f1fc0ebc38029350f3ba8520991b
16sic013j <16sic013@sit.singaporetech.edu.sg>
2018-11-08 03:42:32 +0800

    Fixes

--- anime.js
+++ anime.js
@@ -60,11 +60,19 @@ $(document).ready(function() {
 //session keeper so animation happen once for import fields
 var sec2_p1 = 1;
 var sec2_vid1 = 1;
+var sec4_p1 = 1;
+var sec4_vid1 = 1;
 
-$('.overlay').click(function(){
+$('.sec2_vid1overlay').click(function(){
   $('#sec2_vid1').removeClass('animated pulse infinite');
-  $('#sec2_vid1div1').removeClass('overlay');
+  $('#sec2_vid1div1').removeClass('sec2_vid1overlay');
   sec2_vid1 = 0;
+
+});
+$('.sec4_vid1overlay').click(function(){
+  $('#sec4_vid1').removeClass('animated pulse infinite');
+  $('#sec4_vid1div1').removeClass('sec4_vid1overlay');
+  sec4_vid1 = 0;
 
 });
 
@@ -109,6 +117,18 @@ $(window).scroll(function() {
     $('#sec6_h1').removeClass('animated bounceInLeft');
     $('#sec7_h1').removeClass('animated bounceInLeft');
     $('#sec8_h1').removeClass('animated bounceInLeft');
+    if (sec4_p1 === 1) {
+      $('#sec4_p1').textFx({
+        type: 'fadeIn',
+        iChar: 10,
+        iAnim: '150'
+        //Fades in
+      });
+      sec4_p1 = 0;
+    }
+    if (sec4_vid1 === 1) {
+      $('#sec4_vid1').addClass('animated pulse infinite');
+    }
     return;
   } else if (isScrolledIntoView("#section5")) {
     $('#sec2_h1').removeClass('animated bounceInLeft');
@@ -118,6 +138,10 @@ $(window).scroll(function() {
     $('#sec6_h1').removeClass('animated bounceInLeft');
     $('#sec7_h1').removeClass('animated bounceInLeft');
     $('#sec8_h1').removeClass('animated bounceInLeft');
+
+    $('#sec5_card1').addClass('animated bounce');
+    $('#sec5_card2').addClass('animated bounce');
+
     return;
   } else if (isScrolledIntoView("#section6")) {
     $('#sec2_h1').removeClass('animated bounceInLeft');
